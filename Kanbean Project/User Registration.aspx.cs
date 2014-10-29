9360641335f24279d05af24cde59f58ffcca8819
texplosion <chenshen405@yahoo.com>
2014-10-29 23:03:53 +0200

    Create User Registration.aspx.cs

---
+++ Kanbean Project/User Registration.aspx.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace User_Register_GUI
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btnSub_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
